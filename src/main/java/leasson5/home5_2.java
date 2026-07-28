b0b57bed122b87f6deaf563cf8c76924493c5318
sitnich.e <evgeniy.sitnich@gmail.com>
2026-07-29 01:44:24 +0300

    home work 5

---
+++ src/main/java/leasson5/home5_2.java
@@ -0,0 +1,13 @@
+package leasson5;
+
+public class home5_2 {
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        for (int i = 1; i <= 10; i++)
+
+            System.out.println(number + " x " + i + " = " + (number * i));
+
+    }
+}
